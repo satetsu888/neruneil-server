537ad25d90ad71942b19772c28c2086bb36df1d4
shotaro.akiyama <satetsu888@gmail.com>
2014-02-04 22:39:17 +0900

    fix test.pl

--- test.pl
+++ test.pl
@@ -4,20 +4,38 @@ use JSON::XS;
 use URI;
 use Data::Dumper;
 use List::MoreUtils qw/ uniq /;
+use Digest::MD5 qw/ md5_hex /;
 
-my $key = 'ネイル';
+use constant {
+    NUM_OF_LARGE_RESULT => 8,
+};
+
 my @all_image_urls = ();
+my @keys = ();
+my $SAVE_DIR_PATH  = './images/';
+my $MAX_FILE_SIZE  = '1000000';
+my $TIMEOUT        = 5;
+my $MAX_RETRY      = 2;
 
+my $base_key = 'ネイル';
+my @sub_keys = qw/ 赤 青 黄色 緑 バレンタイン クリスマス かわいい カワイイ /;
+my $number_of_page = 5;
 
-for(1..5){
-    my ($image_urls, $next_url) = get_response_from_api_url( built_api_url($key, $_) );
-    push @all_image_urls, @$image_urls;
+for(@sub_keys){
+    push @keys, $base_key.' '.$_;
+}
+
+for my $key (@keys){
+    for(0..$number_of_page - 1){
+        my ($image_urls, $next_url) = get_response_from_api_url( built_api_url($key, $_ * NUM_OF_LARGE_RESULT) );
+        push @all_image_urls, @$image_urls;
+    }
 }
 
 @all_image_urls = uniq @all_image_urls;
-
-warn Data::Dumper::Dumper \@all_image_urls;
-
+for (@all_image_urls){
+    down_load_file($_);
+}
 
 exit;
 
@@ -52,4 +70,13 @@ sub built_api_url {
 
     warn $uri->as_string;
     return $uri->as_string;
+}
+
+sub down_load_file {
+    my $url = shift;
+    my $file_name = $SAVE_DIR_PATH.md5_hex($url . int rand 0xFFFF);
+
+    `wget -O "$file_name" -Q $MAX_FILE_SIZE -T $TIMEOUT -t $MAX_RETRY "$url"`;
+    unlink $file_name if -s "$file_name" <= 0;
+
 }
