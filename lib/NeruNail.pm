a3191328c33b6cefd3bc6543ca79ab02066c4f68
shotaro.akiyama <satetsu888@gmail.com>
2014-02-09 23:19:42 +0900

    Connect handler to sercher

--- lib/NeruNail.pm
+++ lib/NeruNail.pm
@@ -2,17 +2,28 @@ package NeruNail;
 use strict;
 use warnings;
 use utf8;
+use lib 'lib';
 
-
-use Data::Dumper;
+use NeruNail::ImageSearcher::DRE;
 
 sub get_pic {
     my $class = shift;
     my $param = shift;
 
-    # TODO: implements
-    warn Data::Dumper::Dumper $param;
-    return 1;
+    # TODO: validation
+    my $base_image_hash = $param->{base_image_hash};
+    my $offset = $param->{offset};
+    my $limit = $param->{limit};
+
+    # TODO: make factory class
+    my $searcher = NeruNail::ImageSearcher::DRE->new();
+    my $response = $searcher->call(
+        +{
+            image_hash => $base_image_hash,
+        }
+    );
+
+    return [ @{$response}[$offset..$offset + $limit - 1 ] ];
 }
 
 1;
--- t/lib/NeruNail.t
+++ t/lib/NeruNail.t
@@ -9,11 +9,18 @@ BEGIN {
 }
 
 subtest get_pic => sub {
+    my $image_hash = '00866c4e3befd494ac67d85fe0120e60';
+    my $offset = 0;
+    my $limit = 10;
     my $params = {
-        base_image => 'hogehoge',
+        base_image_hash => $image_hash,
+        offset          => $offset,
+        limit           => $limit,
     };
     my $result = NeruNail->get_pic($params);
     ok($result);
+    isa_ok($result, 'ARRAY');
+    is(scalar @{$result}, $limit, 'expected length');
 };
 
 done_testing;
